1d9babcc1dae93cfd35e73de9f22718c3705a48e
Pascal Kilian <pki@shd.de>
2025-12-11 12:43:42 +0100

    Puzzle two day four

---
+++ src/year_2025/day_four/riddle_two/Main.java
@@ -0,0 +1,74 @@
+package year_2025.day_four.riddle_two;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+  public static void main(String[] args) {
+    File input = new File("src/year_2025/day_four/PuzzleInput.txt");
+    List<List<Character>> paperStorage = new ArrayList<>();
+    int accesibleRolls = 0;
+    boolean rollRemoved = false;
+
+    try (BufferedReader br = new BufferedReader(new FileReader(input))) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        char[] positions = line.toCharArray();
+        paperStorage.add(new ArrayList<>());
+        for (char position : positions) {
+          paperStorage.getLast().add(position);
+        }
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    do {
+      rollRemoved = false;
+      for (int x = 0; x < paperStorage.size(); x++) {
+        for (int y = 0; y < paperStorage.get(x).size(); y++) {
+          if (paperStorage.get(x).get(y) == '@'
+              && getPaperrollCount(getSurrounding(paperStorage, x, y)) < 4) {
+            paperStorage.get(x).set(y, '.');
+            rollRemoved = true;
+            accesibleRolls++;
+          }
+        }
+      }
+    }while(rollRemoved);
+
+    System.out.println(accesibleRolls);
+  }
+
+  private static List<Character> getSurrounding(List<List<Character>> paperStorage, int xPos,
+      int yPos) {
+    List<Character> surrounding = new ArrayList<>();
+    for (int x = xPos - 1; x <= xPos + 1; x++) {
+      for (int y = yPos - 1; y <= yPos + 1; y++) {
+        if (x < 0 || x > paperStorage.size() - 1 || y < 0 || y > paperStorage.size() - 1) {
+          surrounding.add('.');
+          continue;
+        } else if (x == xPos && y == yPos) {
+          continue;
+        }
+        surrounding.add(paperStorage.get(x).get(y));
+      }
+    }
+    return surrounding;
+  }
+
+  private static int getPaperrollCount(List<Character> surrounding) {
+    int count = 0;
+    for (char c : surrounding) {
+      if (c == '@') {
+        count++;
+      }
+    }
+    return count;
+  }
+}
